861c9f7cdf149e930763ef4e9fca2bd42b286581
David Gherlovanu <dgherlovanu098890@gmail.com>
2024-01-18 16:49:09 +0200

    Adding the gilroy font as a default font of the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,54 @@
 import "./globals.scss";
+import localFont from "next/font/local";
+
+const gilroy = localFont({
+  src: [
+    // Normal
+    {
+      path: "./fonts/gilroy/Gilroy-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/gilroy/Gilroy-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/gilroy/Gilroy-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/gilroy/Gilroy-Semibold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    // Italic
+    {
+      path: "./fonts/gilroy/Gilroy-BoldItalic.woff2",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "./fonts/gilroy/Gilroy-RegularItalic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./fonts/gilroy/Gilroy-MediumItalic.woff2",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "./fonts/gilroy/Gilroy-SemiboldItalic.woff2",
+      weight: "600",
+      style: "italic",
+    },
+  ],
+  display: "swap",
+  variable: "--font-gilroy",
+});
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -17,8 +67,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <main>{children}</main>
+      <body className={`${gilroy.variable}`}>
+        <main className="font-sans">{children}</main>
       </body>
     </html>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["var(--font-gilroy)", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
